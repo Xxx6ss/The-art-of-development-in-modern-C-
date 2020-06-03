c4019615c86a018cbb9136bb05658557a684114e
Andrew <a-kireev1989@mail.ru>
2020-06-03 23:42:25 +0300

    final version

--- Electronic Book/Electronic Book/main.cpp
+++ Electronic Book/Electronic Book/main.cpp
@@ -13,6 +13,7 @@
 #include <set>
 #include <utility>
 #include <array>
+#include "test_runner.h"
 #include "profile.h"
 
 using namespace std;
@@ -22,81 +23,70 @@ class ReadingManager {
 public:
   ReadingManager() {
       std::fill(pages.begin(), pages.begin() + MAX_USER_COUNT_, 0);
+      std::fill(users.begin(), users.begin() + MAX_USER_COUNT_, 0);
+      std::fill(rating.begin(), rating.begin() + MAX_USER_COUNT_, 0);
   }
 
   void Read(int user_id, int page_count) {
-      if (pages[user_id] == 0)
+//      if (page_count > MAX_PAGES_)
+//          return ;
+      if (users[user_id] == 0)
           ++users_amount;
       else {
-          users[pages[user_id]].erase(user_id);
+          --pages[users[user_id]];
       }
-      users[page_count].insert(user_id);
-      pages[user_id] = page_count;
+      users[user_id] = page_count;
+      ++pages[page_count];
+      if (page_count < page_read)
+          page_read = page_count;
   }
 
-  double Cheer(int user_id) const {
-      
-      if (pages[user_id] == 0) {
+  double Cheer(int user_id)  {
+
+      if (users[user_id] == 0) {
           return 0;
           }
-      auto it = users.find(pages[user_id]);
       if (users_amount == 1)
           return 1.0;
-      if (it == users.begin())
+      if (users[user_id] <= page_read)
           return 0.0;
+
+       rating[users[user_id]] = count_rating(users[user_id]);
       
-      int count = 0;
-      for(auto iter = users.begin(); iter != it; ++iter) {
-          count += iter->second.size();
-      }
-      if (count == 0)
-          return 0;
-      return (count) * 1.0 / (users_amount - 1);
+      return rating[users[user_id]];
   }
 
 private:
-  static const int MAX_USER_COUNT_ = 100'000;
+  static const int MAX_PAGES_ = 1000;
+  static const int MAX_USER_COUNT_ = 100'001;
 
-    map<int, set<int>> users;
+    array<int, MAX_USER_COUNT_> users;
     array<int, MAX_USER_COUNT_> pages;
+    array<float, MAX_USER_COUNT_> rating;
     int users_amount = 0;
+    int page_read = 99999999;
+    
+
+    float count_rating(const int page) const {
+        int count = 0;;
+        for(int i = 0; i < page; ++i) {
+            count += pages[i];
+        }
+        auto num = (count) * 1.0 / (users_amount - 1);
+//        cout << num << endl;
+        return num;
+    }
 
 };
 
-
-void time_test() {
-    
-    ReadingManager manager;
-    
-    {
-        LOG_DURATION("READ");
-        for (int i = 0; i < 10000; ++i) {
-            manager.Read(i, i * 120);
-        }
-    }
-    
-    
-    {
-         LOG_DURATION("CHEER");
-        for (int i = 0; i < 10000; ++i) {
-            manager.Cheer(i);
-        }
-    }
-    
-    
-}
-
-
-
 int main() {
+  // tests
 
   // Для ускорения чтения данных отключается синхронизация
   // cin и cout с stdio,
   // а также выполняется отвязка cin от cout
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-    
-//    time_test();
 
   ReadingManager manager;
 
@@ -108,7 +98,7 @@ int main() {
     cin >> query_type;
     int user_id;
     cin >> user_id;
-      
+
     if (query_type == "READ") {
       int page_count;
       cin >> page_count;
