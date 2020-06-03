d0bed3f5c0a1d269110987054a509bc972d9477f
Andrew <a-kireev1989@mail.ru>
2020-06-03 12:46:58 +0300

    rewrote using map, set

--- Electronic Book/Electronic Book/main.cpp
+++ Electronic Book/Electronic Book/main.cpp
@@ -10,100 +10,58 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <set>
 #include <utility>
 #include <array>
 #include "profile.h"
 
 using namespace std;
 
-//class User {
-//public:
-//    User() = default;
-//    User(int id, int readed) : id_(id), pages_readed_(readed) {}
-//    int id_ = 0;
-//    int pages_readed_ = 0;
-//    bool operator< (const User& lhs, const User& rhs) const {
-//     return lhs.pages_readed_ < rhs.pages_readed_;
-//    }
-//};
-
-
-
 
 class ReadingManager {
 public:
-  ReadingManager(){
-      std::fill(users.begin(), users.begin() + MAX_USER_COUNT_, 5555544);
-     }
+  ReadingManager() {
+      std::fill(pages.begin(), pages.begin() + MAX_USER_COUNT_, 0);
+  }
 
   void Read(int user_id, int page_count) {
-    
-//    User user(user_id, page_count);
-      users[user_id] = page_count;
-//      cout << users[user_id] << endl;
+      if (pages[user_id] == 0)
+          ++users_amount;
+      else {
+          users[pages[user_id]].erase(user_id);
+      }
+      users[page_count].insert(user_id);
+      pages[user_id] = page_count;
   }
 
   double Cheer(int user_id) const {
-      if (users[user_id] == 5555544) {
-          return 0.0;
-    }
       
-      int pages  = users[user_id];
-      auto count = count_pages(pages);
-      return count;
+      if (pages[user_id] == 0) {
+          return 0;
+          }
+      auto it = users.find(pages[user_id]);
+      if (users_amount == 1)
+          return 1.0;
+      if (it == users.begin())
+          return 0.0;
+      
+      int count = 0;
+      for(auto iter = users.begin(); iter != it; ++iter) {
+          count += iter->second.size();
+      }
+      if (count == 0)
+          return 0;
+      return (count) * 1.0 / (users_amount - 1);
   }
 
 private:
-  // Статическое поле не принадлежит какому-то конкретному
-  // объекту класса. По сути это глобальная переменная,
-  // в данном случае константная.
-  // Будь она публичной, к ней можно было бы обратиться снаружи
-  // следующим образом: ReadingManager::MAX_USER_COUNT.
   static const int MAX_USER_COUNT_ = 100'000;
 
-  array<int, MAX_USER_COUNT_> users;
-    
-    
-    float count_pages(int pages) const{
-        int less = 0;
-        int is_equal = 0;
-        int not_null = 0;
-        for(int pag : users) {
-            if (pag < pages && pag != 5555544)
-                ++less;
-            if (pag == 5555544)
-                ++is_equal;
-            else
-                ++not_null;
-        }
-        
-        
-        if (is_equal == MAX_USER_COUNT_ - 1) {
-//            cout << "op" << endl;
-            return 1;
-            
-        }
-//        cout << "less " <<less << " pages " <<  pages<<endl;
-        if (less == 0)
-            return 0;
-        return less * 1.0 / (not_null - 1);
-    }
+    map<int, set<int>> users;
+    array<int, MAX_USER_COUNT_> pages;
+    int users_amount = 0;
 
-
-//  int GetUserCount() const {
-//    return sorted_users_.size();
-//  }
-//
-//  void SwapUsers(int lhs_position, int rhs_position) {
-//    const int lhs_id = sorted_users_[lhs_position];
-//    const int rhs_id = sorted_users_[rhs_position];
-//    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-//    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-//  }
 };
-
-
-
 
 
 void time_test() {
