b4ec22fcf9ebb12b70fb5c74a0dd9b9278b09aba
Andrew <a-kireev1989@mail.ru>
2020-09-09 18:56:10 +0300

    passed solution for "Expresses"

---
+++ Brown_belt/Expresses/Expresses/main.cpp
@@ -0,0 +1,66 @@
+//
+//  main.cpp
+//  Expresses
+//
+//  Created by Andrew Kireev on 09.09.2020.
+//  Copyright © 2020 Andrew Kireev. All rights reserved.
+//
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+class RouteManager {
+public:
+  void AddRoute(int start, int finish) {
+    reachable_lists_[start].emplace(finish);
+    reachable_lists_[finish].emplace(start);
+  }
+  int FindNearestFinish(int start, int finish) const {
+    int result = abs(start - finish);
+    if (reachable_lists_.count(start) < 1) {
+        return result;
+    }
+    const set<int>& reachable_stations = reachable_lists_.at(start);
+      auto iter_finish = reachable_stations.lower_bound(finish);
+      
+      if (iter_finish != reachable_stations.end())
+          result = min(result, abs(finish - *iter_finish));
+      if (iter_finish != reachable_stations.begin())
+          result = min(result, abs(finish - *prev(iter_finish)));
+    return result;
+  }
+    
+    
+private:
+  unordered_map<int, set<int>> reachable_lists_;
+};
+
+
+int main() {
+  RouteManager routes;
+
+  int query_count;
+  cin >> query_count;
+
+  for (int query_id = 0; query_id < query_count; ++query_id) {
+    string query_type;
+    cin >> query_type;
+    int start, finish;
+    cin >> start >> finish;
+    if (query_type == "ADD") {
+      routes.AddRoute(start, finish);
+    } else if (query_type == "GO") {
+      cout << routes.FindNearestFinish(start, finish) << "\n";
+    }
+  }
+
+  return 0;
+}
